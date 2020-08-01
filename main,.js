965098d4a75189d2d5d1f3a892fad80fa0c9071f
虎鱼 <1255120861@qq.com>
2020-08-01 23:24:26 +0800

    123

--- main,.js
+++
@@ -1,7 +0,0 @@
-setTimeout(()=>{
-    console.log("欢迎来到Git世界");
-})
-
-
-let str = "找抽是不？"
-const arr = ["外国人把那京剧，叫做'Beijing opera'"];
---
+++ main.js
@@ -0,0 +1,21 @@
+setTimeout(()=>{
+    console.log("欢迎来到Git世界");
+})
+
+
+let str = "找抽是不？"
+const arr = ["外国人把那京剧，叫做'Beijing opera'"];
+
+
+class whsj {
+    constructor(name){
+        this.name = name;
+    }
+    details(){
+        console.log(this.name);
+    }
+}
+
+let p = new whsj("关羽，张飞，赵云，马超，黄忠");
+
+p.details();
